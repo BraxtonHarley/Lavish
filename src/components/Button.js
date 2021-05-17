2d56941026881f8502dedea818f18d3aac1f06fb
Braxton Harley <braxton.2012.harley@gmail.com>
2021-05-17 08:53:09 -0500

    building buttons for nav

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../components/Button.css';
+
+const Button = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Button
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,6 +21,40 @@ const Navbar = () => {
           <div class="menu-icon" onClick={handleClick}>
             {click ? <FaTimes /> : <FaBars />}
           </div>
+          
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+                <Link className="nav-links" path="/">
+                    Home
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-links" path="/services">
+                    Services
+                </Link>
+            </li>
+             <li className="nav-item">
+                <Link className="nav-links" path="/products">
+                    Products
+                </Link>
+            </li>
+           
+            <li className="nav-btn">
+                {button ? (
+                    <Link to='/sign-up' className="btn-link" >
+                        <Button buttonStyle='btn--outline'>
+                            Sign Up
+                        </Button>
+                    </Link>
+                ): (
+                
+                    <Link to='/sign-up' className="btn-link" >
+                        <Button buttonStyle='btn--outline' buttonSize='btn--mobile'>
+                         Sign Up
+                        </Button>
+                    </Link>)}
+            </li>
+          </ul>
         </div>
       </div>
     </>
